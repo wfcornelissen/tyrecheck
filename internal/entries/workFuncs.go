bca5f83dd79b208b7be030faf3cc34b2e6eb3737
wfcornelissen <wfcornelissen@gmail.com>
2025-03-27 15:57:47 +0200

    Removed defunct function that was previously commented out.

--- internal/entries/workFuncs.go
+++ internal/entries/workFuncs.go
@@ -8,31 +8,6 @@ import (
 	"github.com/wfcornelissen/tyrecheck/internal/models"
 )
 
-// Defunct in this branch
-/*func CheckTyre(tyrePosition string) error {
-	fmt.Println("Logging tyre check")
-	db, err := sql.Open("sqlite3", "./tyrecheck.db")
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS TyreChecks (tyrePosition TEXT, checkDate TEXT, checkType TEXT)")
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Exec("Insert into TyreChecks (tyrePosition, checkDate, checkType) values (?, ?, ?)", tyrePosition, time.Now(), "Check")
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Tyre check logged")
-
-	return nil
-}
-*/
-
 func CheckTyre() error {
 	tyreCheck := models.TyreCheck{
 		TyreID:    ReadString("Please enter tyre ID: "),
